b5d27796348594c669e8f32d9874bb1453f7fe6b
Fernando Petros <fernandopetros@gmailom>
2022-07-11 01:09:26 +0100

    Initial Setup

---
+++ lua/globals/remaps.lua
@@ -0,0 +1,11 @@
+options = { noremap = true }
+
+-- Change panes on vim ctrl mappings
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', options)
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', options)
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', options)
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', options)
+
+-- Toggle Interface
+vim.api.nvim_set_keymap('n', '<C-b>', ':NvimTreeToggle<CR>', options)
+
