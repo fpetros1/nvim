41d649a9d08a8401dc9d9dbd2ad414e66856c259
Fernando Petros <fernandopetros@gmail.com>
2024-10-20 16:46:50 +0100

    fix: close mini_files before opening lazygit

--- after/plugin/lazygit.lua
+++ after/plugin/lazygit.lua
@@ -1,1 +1,8 @@
-vim.keymap.set('n', '<leader>ggg', '<cmd>LazyGit<CR>', { desc = 'Open Lazygit' })
+local has_minifiles, minifiles = pcall(require, 'mini.files')
+
+vim.keymap.set('n', '<leader>ggg', function()
+    if has_minifiles then
+        minifiles.close()
+    end
+    vim.cmd('LazyGit')
+end, { desc = 'Open Lazygit' })
