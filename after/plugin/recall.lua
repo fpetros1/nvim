35029a3ea36ec9433aec237476cbfca99c1db5ee
Fernando Petros <fernandopetros@gmail.com>
2024-10-20 02:54:02 +0100

    fixed errors

--- after/plugin/recall.lua
+++ after/plugin/recall.lua
@@ -1,11 +1,11 @@
-local hasRecall, recall = pcall(require, "recall")
-
-if hasRecall then
-    recall.setup({})
-
-    vim.keymap.set("n", "m", function() end, { silent = true })
-    vim.keymap.set("n", "<leader>mm", recall.toggle, { noremap = true, silent = true, desc = "Create Mark" })
-    vim.keymap.set("n", "<leader>mn", recall.goto_next, { noremap = true, silent = true, desc = "Next Mark" })
-    vim.keymap.set("n", "<leader>mp", recall.goto_prev, { noremap = true, silent = true, desc = "Previous Mark" })
-    vim.keymap.set("n", "<leader>mc", recall.clear, { noremap = true, silent = true, desc = "Clear Marks" })
-end
+-- local hasRecall, recall = pcall(require, "recall")
+--
+-- if hasRecall then
+--     recall.setup({})
+--
+--     vim.keymap.set("n", "m", function() end, { silent = true })
+--     vim.keymap.set("n", "<leader>mm", recall.toggle, { noremap = true, silent = true, desc = "Create Mark" })
+--     vim.keymap.set("n", "<leader>mn", recall.goto_next, { noremap = true, silent = true, desc = "Next Mark" })
+--     vim.keymap.set("n", "<leader>mp", recall.goto_prev, { noremap = true, silent = true, desc = "Previous Mark" })
+--     vim.keymap.set("n", "<leader>mc", recall.clear, { noremap = true, silent = true, desc = "Clear Marks" })
+-- end
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,33 +1,33 @@
-local hasTelescope, telescope = pcall(require, 'telescope')
-
-if hasTelescope then
-    local builtin = require('telescope.builtin')
-
-    local config = {
-        defaults = {
-            layout_strategy = 'vertical',
-            layout_config = { height = 0.99, width = 0.99 },
-            path_display = { "smart" },
-        },
-        extensions = {
-            ["ui-select"] = {
-                require("telescope.themes").get_dropdown {}
-            }
-        }
-    }
-
-    if vim.g.neovide then
-        config.defaults.winblend = 100
-    end
-
-
-    telescope.setup(config)
-
-    vim.keymap.set('n', '<leader><leader>', builtin.find_files)
-    vim.keymap.set('n', '<leader>P', function() builtin.find_files({ no_ignore = true }) end)
-    vim.api.nvim_set_keymap('n', '<leader>/', '<cmd>Telescope live_grep<cr>', options)
-    vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', options)
-    vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', options)
-
-    require("telescope").load_extension("ui-select")
-end
+-- local hasTelescope, telescope = pcall(require, 'telescope')
+--
+-- if hasTelescope then
+--     local builtin = require('telescope.builtin')
+--
+--     local config = {
+--         defaults = {
+--             layout_strategy = 'vertical',
+--             layout_config = { height = 0.99, width = 0.99 },
+--             path_display = { "smart" },
+--         },
+--         extensions = {
+--             ["ui-select"] = {
+--                 require("telescope.themes").get_dropdown {}
+--             }
+--         }
+--     }
+--
+--     if vim.g.neovide then
+--         config.defaults.winblend = 100
+--     end
+--
+--
+--     telescope.setup(config)
+--
+--     vim.keymap.set('n', '<leader><leader>', builtin.find_files)
+--     vim.keymap.set('n', '<leader>P', function() builtin.find_files({ no_ignore = true }) end)
+--     vim.api.nvim_set_keymap('n', '<leader>/', '<cmd>Telescope live_grep<cr>', options)
+--     vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', options)
+--     vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', options)
+--
+--     require("telescope").load_extension("ui-select")
+-- end
