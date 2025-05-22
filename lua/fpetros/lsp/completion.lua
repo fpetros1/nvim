2ff3653aa1c1436aa68614b214fb6d82c49bf157
Fernando Petros <fernandopetros@gmail.com>
2025-05-22 21:59:07 +0100

    set blink keymap as super tab instead of enter

--- lua/fpetros/lsp/completion.lua
+++ lua/fpetros/lsp/completion.lua
@@ -12,7 +12,34 @@ M.setup = function()
     end
 
     blink.setup({
-        keymap = { preset = 'enter' },
+        keymap = {
+            --super-tab
+            ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+            ['<C-e>'] = { 'hide', 'fallback' },
+
+            ['<Tab>'] = {
+                function(cmp)
+                    if cmp.snippet_active() then
+                        return cmp.accept()
+                    else
+                        return cmp.select_and_accept()
+                    end
+                end,
+                'snippet_forward',
+                'fallback'
+            },
+            ['<S-Tab>'] = { 'snippet_backward', 'fallback' },
+
+            ['<Up>'] = { 'select_prev', 'fallback' },
+            ['<Down>'] = { 'select_next', 'fallback' },
+            ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
+            ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
+
+            ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+            ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+
+            ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+        },
         appearance = {
             use_nvim_cmp_as_default = true,
             nerd_font_variant = 'mono',
@@ -20,7 +47,7 @@ M.setup = function()
         completion = {
             list = {
                 selection = {
-                    preselect = false,
+                    preselect = true,
                     auto_insert = true
                 }
             },
